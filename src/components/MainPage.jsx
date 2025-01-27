b405406b461d7f2972fc42343d626b232839165e
mervesekerfilizoglu <sekermer33@gmail.com>
2025-01-28 01:09:06 +0300

    Main jsx ve css dosyaları oluşturuldu.

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { useHistory } from "react-router-dom";  // useHistory hook
+
+import "./MainPage.css";  
+import "./Header.css";  
+
+export default function MainPage() {
+    const history = useHistory();  // useHistory hook'u ile history nesnesini al
+
+    const handleClick = () => {
+       
+        // Butona tıkladığında 'PizzaOrderForm' sayfasına yönlendir
+        history.push("/PizzaOrderForm");  // History.push ile yönlendir
+    };
+
+   
+
+    return (
+        <div className="form-container">
+                <p className="form-subtitle">KOD ACIKTIRIR PİZZA, DOYURUR</p>
+                <button onClick={handleClick}  className="form-submit">
+                    ACIKTIM
+                </button>
+           
+        </div>
+    );
+}
+
