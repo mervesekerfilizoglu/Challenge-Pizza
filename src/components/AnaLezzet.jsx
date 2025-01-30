f6ce878f7a328701ef2136dfb780ab5051a7610c
mervesekerfilizoglu <sekermer33@gmail.com>
2025-01-30 19:17:00 +0300

    Lezzetus" ve "ana lezzet" sayfaları oluşturuldu.

---
+++ src/components/AnaLezzet.jsx
@@ -0,0 +1,33 @@
+export default function AnaLezzet ()
+{
+
+
+    return(
+        <div className="anaLezzet">
+            <div className="icon-div2">
+                <p>Yeni! Kore</p>
+                <img src="images\iteration-2-images\icons\1.svg" alt="" />
+            </div>
+            <div className="icon-div2 selected">
+                <p>Pizza</p>
+                <img src="images\iteration-2-images\icons\2.svg" alt="" />
+            </div>
+            <div className="icon-div2">
+                <p>Burger</p>
+                <img src="images\iteration-2-images\icons\3.svg" alt="" />
+            </div>
+            <div className="icon-div2">
+                <p>Kızartmalar</p>
+                <img src="images\iteration-2-images\icons\4.svg" alt="" />
+            </div>
+            <div className="icon-div2">
+                <p>Fast food</p>
+                <img src="images\iteration-2-images\icons\5.svg" alt="" />
+            </div>
+            <div className="icon-div2">
+                <p>Gazlı İçecek</p>
+                <img src="images\iteration-2-images\icons\6.svg" alt="" />
+            </div>
+        </div>
+    )
+}
